7088938611d9d47108c05c4f2226db6dca268482
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-10-03 08:42:20 -0400

    js updated for contact page (doesn't work...)

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,76 @@
+// VARIABLES >>>
+// DOM selectors
+let emailInput = document.querySelector('#user-email');
+let messageInput = document.querySelector('#user-message');
+let submitButton = document.querySelector('#contact-submit-button');
+// <<< VARIABLES
+
+
+// FUNCTIONS >>>
 function validateEmail() {
-	let emailInput = document.querySelector('#user-email');
-	let value = emailInput.value;
+    let value = emailInput.value;
 
-	if (!value) {
-		showErrorMessage(emailInput, 'Email is a required field.');
-		return false;
-	}
+    if (!value) {
+        showErrorMessage(emailInput, 'Email is a required field.');
+        return false;
+    }
 
-	if (value.indexOf('@') === -1) {
-		showErrorMessage(emailInput, 'You must enter a valid email address.');
-		return false;
-	}
+    if (value.indexOf('@') === -1) {
+        showErrorMessage(emailInput, 'Please enter a valid email address.');
+        return false;
+    }
 
-	showErrorMessage(emailInput, null);
-	return true;
+    showErrorMessage(emailInput, null);
+    return true;
 }
 
-function validatePassword() {
-	let value = passwordInput.value;
-	
-	if (!value) {
-		showErrorMessage(passwordInput, 'Password is a required field.');
-		return false
-	}
+function validateMessage() {
+    let value = messageInput.value;
 
-	if (value.length < 8) {
-		showErrorMessage(passwordInput, 'Password needs to be at least 8 characters long.');
-		return false
-	}
+    if (!value) {
+        showErrorMessage(messageInput, 'Please include a body in your message.');
+        return false
+    }
 
-	showErrorMessage(passwordInput, null);
-	return true
+    if (value.length > 2) {
+        showErrorMessage(messageInput, 'Message must be under 240 characters.');
+        return false
+    }
+
+    showErrorMessage(messageInput, null); // remove existing error before returning true
+    return true
 }
 
 function showErrorMessage(input, message) {
-	let container = input.parentElement;
+    let container = input.parentElement;
 
-	// Remove existing error
-	let error = container.querySelector('.error-message');
-	if (error) {
-		container.removeChild(error);
-	}
+    // Remove existing error
+    let error = container.querySelector('.error-message');
+    if (error) {
+        container.removeChild(error);
+    }
 
-	// Adds error message if the message isn't empty
-	if (message) {
-		let error = document.createElement('div');
-		error.classList.add('error-message');
-		error.innerText = message;
-		container.appendChild(error);
-	}
+    // Adds error message if the message isn't empty
+    if (message) {
+        console.log(`error: ${message}`)
+        let error = document.createElement('div');
+        error.classList.add('error-message');
+        error.innerText = message;
+        container.appendChild(error);
+    }
 }
+// <<< FUNCTIONS
+
+
+// EVENT LISTENERS >>>
+submitButton.addEventListener('click', () => {
+    let emailValid = validateEmail();
+    let messageValid = validateMessage();
+
+    if (emailValid && messageValid) {
+        console.log(`message sent! ${messageInput.value}`)
+    } else {
+        alert('Spell not found! Please check your... "spell"-ing.'); // Error message if spell not found
+    }
+});
+// <<< EVENT LISTENERS
